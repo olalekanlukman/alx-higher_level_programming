6821757d456cd7d6ca5a86a5d6fe03dcbd7b3f7e
Lukman Mohammed <olalekanlukmanmohammed@yahoo.com>
2021-09-08 05:36:23 -0700

    1 membered list

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -7,11 +7,13 @@ int check_cycle(listint_t *list)
 	if (list == NULL)
 		return (0);
 	current = list;
+	if (list == current->next)
+		return (0);
 	while (current->next != NULL)
 	{
 		current = current->next;
 		if (current->next == list)
-			return 1;
+			return (1);
 	}
-	return 0;
+	return (0);
 }
