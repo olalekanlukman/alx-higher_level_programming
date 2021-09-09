565da5ad86383cc2dd6f2a879450dbc5a2fee8be
Lukman Mohammed <olalekanlukmanmohammed@yahoo.com>
2021-09-09 13:25:18 -0700

    range added

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 if __name__ = "__main__":
-    import hidden_4
+    from hidden_4 import *
 
-names = dir(hidden_4)
+names = dir()
 
-for name in names:
-    if name[:2] != "__":
-        print(name)
+for i in range(0, len(names)):
+    if name[i][:2] != "__":
+        print("{}".format(name[i]))
