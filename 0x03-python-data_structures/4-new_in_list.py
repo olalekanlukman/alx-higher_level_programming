a149f3394119075bb2c606b91e562785dc1d58f8
Lukman Mohammed <olalekanlukmanmohammed@yahoo.com>
2021-09-12 12:17:04 -0700

    if added for idx condition

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,6 +3,8 @@
 
 def new_in_list(my_list, idx, element):
     if my_list:
+        if idx < 0 or idx => len(list):
+            return my_list
         x = []
         for i in my_list:
             x.append(i)
