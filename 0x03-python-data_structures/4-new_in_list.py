04bfb243d10c91c49c12f5f1744e39e0d9f3c56d
Lukman Mohammed <olalekanlukmanmohammed@yahoo.com>
2021-09-12 12:02:27 -0700

    if condition added

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 
+
 def new_in_list(my_list, idx, element):
-    x = []
-    for i in my_list:
-        x.append(i)
-    x[idx] = element
-    return x
+    if my_list:
+        x = []
+        for i in my_list:
+            if i != my_list[idx]:
+                x.append(i)
+            else:
+                x[idx] = element
+        return x
