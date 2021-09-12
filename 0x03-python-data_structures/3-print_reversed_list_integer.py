79ab531ef1a31c1374a32aa1b90ae0f511e9481f
Lukman Mohammed <olalekanlukmanmohammed@yahoo.com>
2021-09-12 11:48:57 -0700

    Null condition added

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,7 +2,7 @@
 
 
 def print_reversed_list_integer(my_list=[]):
-    if mylist:
+    if my_list:
         i = len(my_list) - 1
         while (i >= 0):
             print("{:d}".format(my_list[i]))
