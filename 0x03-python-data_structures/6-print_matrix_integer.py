587a38d27e69c2975f4d577575ca5022b064cc7f
Lukman Mohammed <olalekanlukmanmohammed@yahoo.com>
2021-09-12 12:33:51 -0700

    fix the print function

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,4 +3,5 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for i in range(len(row)):
-            print(row[i])()
+            print("{:d}".format(row[i]), end=" ")
+        print()
