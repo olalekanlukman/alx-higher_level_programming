2689f0fcd8666d74497dde513ae697e055170829
Lukman Mohammed <olalekanlukmanmohammed@yahoo.com>
2021-09-12 04:21:54 -0700

    * with quote

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import sys
+from calculator_1 import add, sub, mul, div
+
+if len(sys.argv) != 4:
+    print("usage: ./100-my_calculator.py <a> <operator> <b>")
+    exit(1)
+
+operators = ['+', '-', '*', '/']
+math = [add, sub, mul, div]
+for i in range(0, 4):
+    if sys.argv[2] == operators[i]:
+        print("{} {} {} = {}".format(sys.argv[1], operators[i], sys.argv[3],
+              mul(int(sys.argv[1]), int(sys.argv[3]))))
+        break
+
+if sys.argv[2] != operators[i]:
+    print("Unknown operator. Available operators: +, -, * and /")
