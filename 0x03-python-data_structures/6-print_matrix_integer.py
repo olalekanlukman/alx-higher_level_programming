e8324aae06dd6cac83f52c6afca83858901b1b07
Lukman Mohammed <olalekanlukmanmohammed@yahoo.com>
2021-09-12 14:06:19 -0700

    if statement added

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
+
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for i in range(len(row)):
-            print("{:d}".format(row[i]), end=" ")
-        print()
+    if matrix:
+        for row in matrix:
+            for i in range(len(row)):
+                print("{:d}".format(row[i]), end=" " if row[-1] != i else "")
+            print()
