b62dc67a9708bebdd90978aff38b0b1316a434ad
Lukman Mohammed <olalekanlukmanmohammed@yahoo.com>
2021-09-18 13:27:10 -0700

    romanized

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+
+def roman_to_int(roman_string):
+    sum = 0
+    rom_num = dict(I=1, V=5, X=10, L=50, C=100, D=500, M=1000)
+
+    rom_len = len(roman_string)
+    if rom_len == 0:
+        return None
+
+    for i in range(0, rom_len):
+        if roman_string[i] in rom_num:
+            if i < (rom_len - 1):
+                if rom_num[roman_string[i + 1]] and
+                rom_num[roman_string[i]] < rom_num[roman_string[i + 1]]:
+                    sum -= rom_num[roman_string[i]]
+                else:
+                    sum += rom_num[roman_string[i]]
+            else:
+                sum += rom_num[roman_string[i]]
+        else:
+            return None
+
+    return sum
