1809bbf775081cd8b0609823b3cd1bc827c304ed
Lukman Mohammed <olalekanlukmanmohammed@yahoo.com>
2021-09-18 14:56:26 -0700

    forgot ]

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,25 +2,23 @@
 
 
 def roman_to_int(roman_string):
-    sum = 0
+    sum_r = 0
     rom_num = dict(I=1, V=5, X=10, L=50, C=100, D=500, M=1000)
 
     rom_len = len(roman_string)
     if rom_len == 0:
-        return None
-    for x in roman_string:
-        if x not in rom_num:
-            return None
+        return 0
+
     for i in range(0, rom_len):
         if roman_string[i] in rom_num:
             if i < (rom_len - 1):
                 if rom_num[roman_string[i + 1]] and rom_num[roman_string[i]]\
-                        < rom_num[roman_string[i + 1]]:
-                    sum -= rom_num[roman_string[i]]
+                < rom_num[roman_string[i + 1]]:
+                    sum_r -= rom_num[roman_string[i]]
                 else:
-                    sum += rom_num[roman_string[i]]
+                    sum_r += rom_num[roman_string[i]]
             else:
-                sum += rom_num[roman_string[i]]
+                sum_r += rom_num[roman_string[i]]
         else:
-            return None
-    return sum
+            return 0
+    return sum_r
