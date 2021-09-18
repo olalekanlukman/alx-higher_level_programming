ae22302e11bdaee45453bb8b902fbe10bcc802c3
Lukman Mohammed <olalekanlukmanmohammed@yahoo.com>
2021-09-18 13:52:48 -0700

    \ for newline

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -8,12 +8,14 @@ def roman_to_int(roman_string):
     rom_len = len(roman_string)
     if rom_len == 0:
         return None
-
+    for x in roman_string:
+        if x not in rom_num:
+            return None
     for i in range(0, rom_len):
         if roman_string[i] in rom_num:
             if i < (rom_len - 1):
-                if rom_num[roman_string[i + 1]] and
-                rom_num[roman_string[i]] < rom_num[roman_string[i + 1]]:
+                if rom_num[roman_string[i + 1]] and rom_num[roman_string[i]]\
+                        < rom_num[roman_string[i + 1]]:
                     sum -= rom_num[roman_string[i]]
                 else:
                     sum += rom_num[roman_string[i]]
@@ -21,5 +23,4 @@ def roman_to_int(roman_string):
                 sum += rom_num[roman_string[i]]
         else:
             return None
-
     return sum
